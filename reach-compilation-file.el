a8cc0d83fa704d60e92bb7ef0e50fea7444fb6ed
sroccaserra <sroccaserra@yahoo.com>
2009-12-22 18:15:41 +0100

    Added reach-compilation-file

---
+++ reach-compilation-file.el
@@ -0,0 +1,18 @@
+(defvar *compilation-file* "Makefile")
+
+(defun reach-compilation-file ()
+  ;; Search for the compilation file traversing up the directory tree.
+  (let* ((dir (expand-file-name default-directory))
+         (parent (file-name-directory (directory-file-name dir))))
+    (while (and (not (file-readable-p (concat dir *compilation-file*)))
+                (not (string= parent dir)))
+      (setq dir parent
+            parent (file-name-directory (directory-file-name dir))))
+    (if (string= dir parent)
+        (error "Search file %s is missing" *compilation-file*)
+      (with-current-buffer compilation-last-buffer
+        (setq default-directory dir)))))
+
+(setq compilation-process-setup-function 'reach-compilation-file)
+
+(provide 'reach-compilation-file)
