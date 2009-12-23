17ba28a00a539839dc918e3de984ae046299f094
sroccaserra <sroccaserra@yahoo.com>
2009-12-23 14:23:50 +0100

    reach compilation file only when using make

--- reach-compilation-file.el
+++ reach-compilation-file.el
@@ -1,17 +1,19 @@
 (defvar *compilation-file* "Makefile")
 
 (defun reach-compilation-file ()
-  ;; Search for the compilation file traversing up the directory tree.
-  (let* ((dir (expand-file-name default-directory))
-         (parent (file-name-directory (directory-file-name dir))))
-    (while (and (not (file-readable-p (concat dir *compilation-file*)))
-                (not (string= parent dir)))
-      (setq dir parent
-            parent (file-name-directory (directory-file-name dir))))
-    (if (string= dir parent)
-        (error "Search file %s is missing" *compilation-file*)
-      (with-current-buffer compilation-last-buffer
-        (setq default-directory dir)))))
+  (when (string-match "make"
+                      (car compilation-arguments))
+    ;; Search for the compilation file traversing up the directory tree.
+    (let* ((dir (expand-file-name default-directory))
+           (parent (file-name-directory (directory-file-name dir))))
+      (while (and (not (file-readable-p (concat dir *compilation-file*)))
+                  (not (string= parent dir)))
+        (setq dir parent
+              parent (file-name-directory (directory-file-name dir))))
+      (if (string= dir parent)
+          (error "Search file %s is missing" *compilation-file*)
+        (with-current-buffer compilation-last-buffer
+          (setq default-directory dir))))))
 
 (setq compilation-process-setup-function 'reach-compilation-file)
 
