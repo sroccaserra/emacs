0a9f97174276a4f6fb854bbfb9e5ed71d4144131
sroccaserra <sroccaserra@yahoo.com>
2010-02-16 13:04:10 +0100

    Updated path and layout

--- jekyll.el
+++ jekyll.el
@@ -1,9 +1,9 @@
-(defvar *jekyll-post-dir* "~/Developer/github/sroccaserra.github.com/_posts/")
+(defvar *jekyll-post-dir* "~/Developer/sroccaserra.github.com/_posts/")
 
 (defun jekyll:new-post (title)
   (interactive "sTitle: ")
   (let ((dashed-title (replace-in-string (downcase title) " " "-"))
-        (header (format "---\ntitle: %s\nlayout: default\n---\n\n{{ page.title }}\n================\n\n"
+        (header (format "---\ntitle: %s\nlayout: post\n---\n\n{{ page.title }}\n================\n\n"
                         title)))
     (find-file (concat *jekyll-post-dir*
                        (format-time-string "%Y-%m-%d-")
