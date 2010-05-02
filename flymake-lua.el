5ccf8c23eea7d55ac327914b852c4b5e100a2c72
sroccaserra <sroccaserra@yahoo.com>
2010-05-02 23:20:52 +0200

    Added customization for luac program

--- flymake-lua.el
+++ flymake-lua.el
@@ -1,36 +1,41 @@
-;;;
-;; flymake-lua.el
 ;;
-
+;; Flymake for Lua
 ;;
-;; Flymake for lua
+;; Usage:
+;;   (require 'flymake-lua)
+;;   (add-hook 'lua-mode-hook 'flymake-lua-load)
+;;
+;; Note: litterally stolen from Steve Purcell's Flymake Ruby.
+;; See http://github.com/purcell/emacs.d/blob/master/site-lisp/flymake-ruby/flymake-ruby.el
 ;;
 
 (require 'flymake)
 
-;; Not provided by flymake itself, curiously
+(defgroup flymake-lua nil
+  "Flymake Lua Customizations")
+
+(defcustom flymake-luac-program "luac"
+  "How to invoke luac. Other possible value: /usr/local/bin/luac."
+  :type 'string
+  :group 'flymake-lua)
+
 (defun flymake-create-temp-in-system-tempdir (filename prefix)
   (make-temp-file (or prefix "flymake-lua")))
 
-
 (defun flymake-lua-init ()
-  "Invoke luac with '-p' to get syntax checking"
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-in-system-tempdir))
-	 (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "luac" (list "-p" local-file))))
+  (list flymake-luac-program
+        (list "-p" (flymake-init-create-temp-buffer-copy
+                    'flymake-create-temp-in-system-tempdir))))
 
-(push '("\\.lua\\'" flymake-lua-init) flymake-allowed-file-name-masks)
-(push '("^.*luac[0-9.]*\\(.exe\\)?: *\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 2 3 nil 4)
-      flymake-err-line-patterns)
+(defvar flymake-lua-allowed-file-name-masks '(("\\.lua\\'" flymake-lua-init)))
+(defvar flymake-lua-err-line-patterns '(("^.*luac[0-9.]*\\(.exe\\)?: *\\(.*\\):\\([0-9]+\\): \\(.*\\)$"
+                                    2 3 nil 4)))
 
-(add-hook 'lua-mode-hook
-          '(lambda ()
-	     "Don't want flymake mode for lua regions in rhtml
-	      files and also on read only files"
-	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-		 (flymake-mode 1))))
+(defun flymake-lua-load ()
+  (interactive)
+  (when (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+    (set (make-local-variable 'flymake-allowed-file-name-masks) flymake-lua-allowed-file-name-masks)
+    (set (make-local-variable 'flymake-err-line-patterns) flymake-lua-err-line-patterns)
+    (flymake-mode t)))
 
 (provide 'flymake-lua)
