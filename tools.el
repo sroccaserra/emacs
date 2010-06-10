537865331fe277391761316b9fcf8fd80e6a309f
sroccaserra <sroccaserra@yahoo.com>
2010-06-10 22:52:18 +0200

    Added tools.el

---
+++ tools.el
@@ -0,0 +1,96 @@
+;;
+;; Various tool functions & macros
+;;
+
+(defmacro comment (&rest body)
+  "Ignores body, yields nil"
+  nil)
+
+;;;;;;;
+;; Math
+
+(defun mean (values)
+  (/ (reduce '+ values)
+     (float (length values))))
+
+(defun square (x)
+  (* x x))
+
+(defun variance (values)
+  (- (->> values (mapcar 'square) mean)
+     (square (mean values))))
+
+;;;;;;;;;;;;
+;; Sequences
+
+(defun sequence (maybe-seq)
+  "Returns the value wrapped in a sequence if it is not a sequence already."
+  (if (sequencep maybe-seq) maybe-seq
+    (list maybe-seq)))
+
+(defun random-elt (sequence)
+  (elt sequence
+       (-> sequence length random)))
+
+(defun seq-difference (lseq rseq)
+  (remove-if (lambda (element) (find element rseq :test 'equal))
+             lseq))
+
+;;;;;;;;;;
+;; Strings
+
+(defun string-empty-p (str)
+  (unless (stringp str)
+    (error "not a string."))
+  (string= "" str))
+
+(defun string-not-blank-p (str)
+  (when str
+    (not (string-empty-p str))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Undestructive alist functions
+
+(defun alist-get (alist key &optional default)
+  (or (assoc-default key alist)
+      default))
+
+(defun alist-remove (alist key)
+  "Doesn't change the original alist, returns a new one instead."
+  (remove-if (lambda (x) (equal key (car x)))
+             alist))
+
+(defun alist-set (alist key value)
+  "Doesn't change the original alist, returns a new one instead."
+  (cons (cons key value) (alist-remove alist key)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clojure's Trush operators
+
+(defmacro -> (result &rest body)
+  (dolist (form body result)
+    (setq form (sequence form)
+          result (append (list (car form) result)
+                         (cdr form)))))
+
+(defmacro ->> (result &rest body)
+  (dolist (form body result)
+    (setq form (sequence form)
+          result (append form (list result)))))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Functional tools
+
+(defmacro partial (f &rest args)
+  `(lambda (&rest more)
+     (apply ',f ,@args more)))
+
+(defmacro lexdef (name args &rest body)
+  "Defun with lexically-scoped parameters. Could also be called lexical-defun."
+  `(defun ,name ,args
+     (lexical-let ,(->> args
+                        (remove-if (partial equal '&rest))
+                        (mapcar (lambda (arg) (list arg arg))))
+       ,@body)))
+
+(provide 'tools)
