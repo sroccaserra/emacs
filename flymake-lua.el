ee23c427a8eb01773c87e215d0e61cd8b5b5fe76
Sébastien Roccaserra <sroccaserra@yahoo.com>
2014-03-10 10:30:06 +0100

    Switched flymake-lua.el licence to Apache 2 licence (for #3)

--- flymake-lua.el
+++ flymake-lua.el
@@ -1,16 +1,24 @@
 ;;; flymake-lua.el --- Flymake for Lua
 
 ;; Copyright (C) 2009-2014 Sébastien Roccaserra
-     
+
 ;; Author: Sébastien Roccaserra (format "<%s%s@%s.%s>" "s" "roccaserra" "yahoo" "com")
 ;; Created: 07 Dec 2009
 ;; Keywords: lua
 
 ;; This file is not part of GNU Emacs.
 
-;; This file is free software...
-;; ...
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
 
 ;;; Commentary:
 
